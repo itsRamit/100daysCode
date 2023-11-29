29871d3bc58edbc38a7c48559001c166679b244c
ItsRamit <ece22145@iiitkalyani.ac.in>
2023-11-29 22:11:01 +0530

    day 3

--- Blackjack game/index.js
+++ Blackjack game/index.js
@@ -1,41 +1,53 @@
 let firstCard = 10
 let secondCard = 4
-let sum = firstCard + secondCard
+let cards = [firstCard, secondCard]
+let sum = cards[0] + cards[1]
 let hasBlackJack = false
 let isAlive = true
-let message = "Want to play a round?"
-
-let msgEl = document.getElementById("message-el")
+let message = ""
+let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 // let sumEl = document.querySelector("#sum-el") --> can also use this
 
+function randomNumber(){
+    return Math.floor( Math.random()*14 )
+}
+
 function startGame(){
     renderGame()
-}
-
-function renderGame(){
+    }
+    
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for(let i = 0 ; i<cards.length;i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
-    cardsEl.innerText = "Cards: " + firstCard + " " + secondCard
-    sumEl.innerText = "Sum: "+ sum
-    msgEl.innerText = message
-
+    messageEl.textContent = message
 }
 
-if(sum <= 21) function newCard(){
-    let card = 8
-    firstCard = secondCard
-    secondCard = card
+
+function newCard() {
+    let card = randomNumber()
     sum += card
+    cards.push(card)
     renderGame()
 }
+
+
+// let c = Math.random()*21
+// let b = Math.floor(c)
+// console.log(c)
+// console.log(b)
 
