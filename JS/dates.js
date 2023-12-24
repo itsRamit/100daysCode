d2cffb920bfe67a7730bb65142f7ae6aa7c79ac6
ItsRamit <ece22145@iiitkalyani.ac.in>
2023-12-24 20:34:12 +0530

    maths & loops

--- JS/dates.js
+++ JS/dates.js
@@ -1,11 +1,11 @@
-let a = new Date("03-03-2020")  //returns current date & time
-let x = new Date(milliseconds) // returns date & time after the given ms from jan 1 1970
-let y = new Date(dateString) //returns a specific date
-let z = new Date(year, month, day, hours, minutes, seconds, milliseconds) // returns a specific date & time
+// let a = new Date()  //returns current date & time
+// let x = new Date(milliseconds) // returns date & time after the given ms from jan 1 1970
+// let y = new Date(dateString) //returns a specific date
+// let z = new Date(year, month, day, hours, minutes, seconds, milliseconds) // returns a specific date & time
 
 
 
-
+// get methods
 let b = new Date().getDate() // returns current day of month (1-31)
 let c = new Date().getDay()  // returns current day of week (0-6)
 let d = new Date().getFullYear() // returns current year
@@ -16,4 +16,15 @@ let h = new Date().getMinutes() // returns current seconds (0-59)
 let i = new Date().getMonth() // returns current month (0-11)
 let j = new Date().getUTCDate() // returns current UTC day of month (1-31)...and we can implement UTC for all the above methods
 
-console.log(x)
+// set methods
+let k = new Date().setDate(21) // set the given day (0-31)
+let l = new Date().setMonth(2)  // set the given month (0-11)
+let m = new Date().setFullYear(2022,11,31) // returns current year,month,day
+let n = new Date().setHours() // set hours (0-23)
+let o = new Date().setMilliseconds() // set milliseconds (0-999)
+let p = new Date().setSeconds() // set seconds (0-59)
+let q = new Date().setMinutes() // set seconds (0-59)
+
+
+
+console.log(m.to)
---
+++ JS/maths.js
@@ -0,0 +1,8 @@
+
+Math.round(x)	//Returns x rounded to its nearest integer
+Math.ceil(x)	//Returns x rounded up to its nearest integer
+Math.floor(x)	//Returns x rounded down to its nearest integer
+Math.trunc(x)	//Returns the integer part of x (new in ES6)
+
+Math.random() //returns a random number between [0,1):
+x*Math.random() //returns a random number between [0,x):
